801e67658bcf3070ac1989aa0a511c672108bfc5
Matvey Litvin <61112437+xcviko@users.noreply.github.com>
2023-07-03 19:36:44 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+import { StatusBar } from 'expo-status-bar';
+import {useEffect} from "react";
+import {Alert, Platform, StyleSheet, Text, View} from 'react-native';
+import * as Notifications from 'expo-notifications';
+import Constants from "expo-constants";
+
+export default function App() {
+
+useEffect(() => {
+  registerForPushNotificationsAsync().then((token) => {
+    setTimeout(() => Alert.alert(token), 2000)
+  });
+}, [])
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+  Notifications.addNotificationResponseReceivedListener(response => {
+    console.log(response.notification.request.content.data);
+  });
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      console.log('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    console.log('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
+
+
+
+return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
